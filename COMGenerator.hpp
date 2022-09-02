ce7b7e86cf37acca790a82dec84d42f6b5e06e5d
danteking <danteking@aliyun.com>
2022-09-02 15:50:26 +0800

    rewrite BuildFile function

--- COMGenerator.hpp
+++ COMGenerator.hpp
@@ -6,12 +6,12 @@
 #include <unordered_map>
 #include <cstring>
 #include "GJFParser.hpp"
+#include "constant.hpp"
 
 /**
  * @brief 换行符
  * 
  */
-const static std::string LF = "\n";
 const static std::string chk = "%chk=M-opt";
 const static std::string mem = "%mem=2gb";
 const static std::string proc = "%nprocshared=10";
@@ -35,54 +35,27 @@ public:
 
 
     bool BuildFile() {
-        m_file_content += chk;
-        m_file_content += LF;
-        m_file_content += mem;
-        m_file_content += LF;
-        m_file_content += proc;
-        m_file_content += LF;
-        m_file_content += method;
-        m_file_content += LF;
-        m_file_content += LF;
-        m_file_content += a3;
-        m_file_content += LF;
-        m_file_content += LF;
-        m_file_content += m_gjfparser.GetChargeAndSpinMultiplicityLine();
-        m_file_content += LF;
+        m_file_content += (chk + Constant::LF);
+        m_file_content += (mem + Constant::LF);
+        m_file_content += (proc+ Constant::LF);
+        m_file_content += (method + Constant::LF);
+        m_file_content += Constant::LF;
+        m_file_content += (a3 + Constant::LF);
+        m_file_content += Constant::LF;
+        m_file_content += (m_gjfparser.GetChargeAndSpinMultiplicityLine() + Constant::LF);
         m_file_content += m_gjfparser.GetAtomAndCoordinate();
         if(!m_gjfparser.m_transition_element_set.empty()) {
-            std::string main_group_elems;
-            for(auto elem: m_gjfparser.m_main_group_element_set) {
-                main_group_elems += elem;
-                main_group_elems += ' ';
-            }
-            main_group_elems += '0';
-            m_file_content += main_group_elems;
-            m_file_content += LF;
-            m_file_content += "6-31G(d)";
-            m_file_content += LF;
-            m_file_content += "****";
-            m_file_content += LF;
-
-            std::string transition_elems;
-            for(auto elem: m_gjfparser.m_transition_element_set) {
-                transition_elems += elem;
-                transition_elems += ' ';
-            }
-            transition_elems += '0';
-            m_file_content += transition_elems;
-            m_file_content += LF;
-            m_file_content += "LANL2DZ";
-            m_file_content += LF;
-            m_file_content += "****";
-            m_file_content += LF;
-            m_file_content += LF;
-            m_file_content += transition_elems;
-            m_file_content += LF;
-            m_file_content += "LANL2DZ";
-            m_file_content += LF;
+            m_file_content += (BuildMainGroupElementsLine() + Constant::LF);
+            m_file_content += ("6-31G(d)" + Constant::LF);
+            m_file_content += ("****" + Constant::LF);
+            m_file_content += (BuildTransitionElementsLine() + Constant::LF);
+            m_file_content += ("LANL2DZ" + Constant::LF);
+            m_file_content += ("****" + Constant::LF);
+            m_file_content += Constant::LF;
+            m_file_content += (BuildTransitionElementsLine() + Constant::LF);
+            m_file_content += ("LANL2DZ" + Constant::LF);
         }
-        m_file_content += LF;
+        m_file_content += Constant::LF;
     }
 
     bool GenerateFile() {
@@ -97,6 +70,27 @@ public:
             return false;
         }
     }
+protected:
+    std::string BuildMainGroupElementsLine() {
+        std::string main_group_elems;
+        for(auto elem: m_gjfparser.m_main_group_element_set) {
+            main_group_elems += elem;
+            main_group_elems += ' ';
+        }
+        main_group_elems += '0';
+        return main_group_elems;
+    }
+
+    std::string BuildTransitionElementsLine() {
+        std::string transition_elems;
+        for(auto elem: m_gjfparser.m_transition_element_set) {
+            transition_elems += elem;
+            transition_elems += ' ';
+        }
+        transition_elems += '0';
+        return transition_elems;
+    }
+
 protected:
     GJFParser m_gjfparser;
     std::string m_file_content;
